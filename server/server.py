d9f1a7f058fbe12fc4bea57a6c92a9a1a3349e52
Krishna Priya <121747046+Sanchez0902@users.noreply.github.com>
2024-03-28 22:09:01 +0530

    Add files via upload

---
+++ server/server.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify
+
+import util
+
+app = Flask(__name__)
+
+
+@app.route('/get_location_names')
+def get_location_name():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    try:
+        total_sqft = float(request.form['total_sqft'])
+        location = request.form['location']
+        bhk = int(request.form['bhk'])
+        bath = int(request.form['bath'])
+
+        util.load_saved_artifacts()
+
+        response = jsonify({
+            'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+        })
+
+        response.headers.add('Access-Control-Allow-Origin', '*')
+
+        return response
+    except KeyError as e:
+        # Handle missing parameters error
+        response = jsonify({
+            'error': f'Missing parameter: {e.args[0]}'
+        })
+        response.status_code = 400
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+    except Exception as e:
+        # Handle any other errors
+        response = jsonify({
+            'error': 'An error occurred while processing the request'
+        })
+        response.status_code = 500
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    app.run()
